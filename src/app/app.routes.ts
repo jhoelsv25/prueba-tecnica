09ea48eb4c4febfa03d10ba3e6693292def92852
jhoel silvestre <jhoel.sv25@gmail.com>
2023-12-19 16:21:43 -0500

    Add routes for new pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'view-01',
+    loadComponent: () =>
+      import('./pages/view-01/view-01.component').then(
+        (m) => m.View01Component
+      ),
+  },
+  {
+    path: 'view-02',
+    loadComponent: () =>
+      import('./pages/view-02/view-02.component').then(
+        (m) => m.View02Component
+      ),
+  },
+  {
+    path: 'view-03',
+    loadComponent: () =>
+      import('./pages/view-03/view-03.component').then(
+        (m) => m.View03Component
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
