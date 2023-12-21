b6a602542801bca44d62e932141919ebd86dd602
jhoel silvestre <jhoel.sv25@gmail.com>
2023-12-20 20:06:02 -0500

    Update app component layout and add GraphQL provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SideBarComponent } from './layouts/side-bar/side-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, SideBarComponent],
+  imports: [RouterOutlet, SideBarComponent, CountryDetailComponent],
 })
 export class AppComponent {
   title = 'prueba-tecnica';
